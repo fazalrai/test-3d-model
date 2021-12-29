b116bb427b8dd2b7de2174684951d4cd13b31af2
fazarai <fazal.rai456@gmail.com>
2021-12-29 15:28:02 +0500

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SignIn from "./signIn/Index";
+import Header from "./Header";
+import SingUp from "./signUp/Index";
+import ModelList from "./modelList/Index";
+import InvalidPath from "./invalidPath/Index";
 
-function App() {
+import { Routes, Route } from "react-router-dom";
+
+export default function App() {
+  const token = localStorage.getItem("Token");
+  const [login, setLogin] = React.useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header setLogin={setLogin}/>
+      <Routes>
+        <Route exact path="/" element={<SingUp setLogin={setLogin} />} />
+        <Route exact path="/login" element={<SignIn  setLogin={setLogin} />} />
+        {token && <Route path="/models" exact element={<ModelList />} />}
+        <Route path="*" element={<InvalidPath />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
